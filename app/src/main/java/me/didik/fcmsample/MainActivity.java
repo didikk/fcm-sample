f719db778b972ae3e035f7e68969d2ac06caa541
didikk <ismawantodidik@gmail.com>
2016-06-07 12:08:19 +0700

    Initial commit

---
+++ app/src/main/java/me/didik/fcmsample/MainActivity.java
@@ -0,0 +1,13 @@
+package me.didik.fcmsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
